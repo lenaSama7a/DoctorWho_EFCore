f4d214a723791f99e789a04fb4190d4caf6a4ffc
lenaSamaha <lena.sama7a@gmail.com>
2023-02-19 21:04:40 +0200

    Adding requirement 7

--- DoctorWho.Db/Migrations/20230219190303_SeedingTables.Designer.cs
+++ DoctorWho.Db/Migrations/20230219190303_SeedingTables.Designer.cs
@@ -4,6 +4,7 @@ using DoctorWho.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoctorWho.Db.Migrations
 {
     [DbContext(typeof(DoctorWhoCoreDbContext))]
-    partial class DoctorWhoCoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230219190303_SeedingTables")]
+    partial class SeedingTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
