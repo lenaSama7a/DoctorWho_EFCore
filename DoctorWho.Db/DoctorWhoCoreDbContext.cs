65d01e1ef17351f745d8bcb13430e23426b5d67b
lenaSamaha <lena.sama7a@gmail.com>
2023-02-19 20:19:26 +0200

    Adding the first five requirements

---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Companion> Companions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<EpisodeCompanion> EpisodeCompanions { get; set; }
+        public DbSet<EpisodeEnemy> EpisodeEnemies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Data Source =DESKTOP-M189RU0;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog = DoctorWhoCore"
+                );
+        }
+        
+    }
+}
